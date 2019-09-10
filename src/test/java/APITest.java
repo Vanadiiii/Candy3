1ada0363b22e905ce1eb8333e0d07846eb8e5591
Vanadiiii <ivan.matveev94@mail.ru>
2019-09-10 14:12:50 +0400

    2.0 It's ready!

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -1,26 +1,33 @@
+import com.sun.xml.internal.ws.client.ResponseContext;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 
 
 public class APITest {
-    private static String baseURI = "https://httpbin.org/";
-    //    public someMethod(){};
-
-    public void apiRequest(int someStatus){
+    private void apiRequest(int someStatus){
+        String baseURI = "https://httpbin.org/";
         RestAssured.given()
                 .baseUri(baseURI) //путь к домену
                 .basePath("status/" + someStatus) //Вывести статус в ответе
 
                 .when().delete()// какой метод я для этого использую
                 .then()
-                .statusCode(someStatus);
+                .statusCode(someStatus);//проверяю статус ответа
+
     }
     @Test
     public void apiTest(){
         int[] statusMassive = {200, 300, 404, 500};
-        for (int statusNumber = 0; statusNumber< statusMassive.length; statusNumber++) {
-            apiRequest(statusMassive[statusNumber]);
-            System.out.println("StatusCode " + statusMassive[statusNumber] + " is responded");
+        for (int i : statusMassive) {
+            try {
+                apiRequest(i);
+                System.out.println("StatusCode " + i + " is responded");
+            } catch (AssertionError e) {
+                System.out.println("StatusCode " + i + " is NOT responded");
+            }
         }
     }
 }
